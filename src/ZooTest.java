558ab6b6839ca73469e92809127de924a5e155d3
Sierra Budinoff <sierra.j.budinoff-1@ou.edu>
2019-02-07 14:19:11 -0600

    pass Zoo Capacity and Height Tests

--- src/ZooTest.java
+++ src/ZooTest.java
@@ -1,10 +1,32 @@
 
 public class ZooTest {
+	Zoo try3 = new Zoo(2);
+		Animal i = new Animal ("blue", "Frank", 23.1, 11.9);
 	
-	public static void main(String[] args) {
-		// Zoo try0 = new Zoo(0);
-		// Zoo try1 = new Zoo(1);
+	public void capacityTest() throws AssertException {
+		Zoo try1 = new Zoo(0);
+		// capacity should be 0
+		 Assert.assertEquals(0, try1.getCapacity());
+		 
+		 Zoo try2 = new Zoo(10);
+		 Assert.assertEquals(10, try2.getCapacity());
+		}
 
+	public void getTotalHeightTest() throws AssertException {
 		
+		Animal j = new Animal ("purple", "Bill", 298.3, 89.3);
+		Animal k = new Animal("cyan", "Ryan", 7.1, 8.0);
+		try3.addAnimal(i);
+		try3.addAnimal(j);
+		try3.addAnimal(k);
+		
+		 Assert.assertEquals(109.2, try3.getTotalHeight());
 	}
+		
+	public void toStringTest() throws AssertException {
+		try3.addAnimal(i);
+		 Assert.assertEquals("These animals live in the zoo: \nFrank, a blue-colored animal. 23.1 pounds, 11.9 inches\n", try3.toString());
+	}
+		
+	
 }
