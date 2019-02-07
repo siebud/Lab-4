b0fc40ec61189664f264e15fbe30961b94534f52
Sierra Budinoff <sierra.j.budinoff-1@ou.edu>
2019-02-07 10:07:35 -0600

    First commit

---
+++ src/Todo.java
@@ -0,0 +1,42 @@
+public class Todo {/*
+Lab Section_________: 13
+Last Modified_______: 2019-07-02
+OU ID_______________: 113471919
+OU EMAIL____________: sierra.j.budinoff-1@ou.edu
+
+Tasks:
+1. set up git repository
+2. initial commit
+3. Zoo height and capacity test
+4. zoo toString test 
+5. Animal constructor/variable tests
+6. Animal toString test
+7. Animal equals method
+8. Equipment variable tests
+9. Equipment string tests
+10. Equipment equals method
+
+Estimated Time (minutes):
+1. 5
+2. 8
+3. 30
+4. 30
+5. 40
+6. 40
+7. 50
+8. 45
+9. 45
+10. 50
+
+Actual Time (minutes):
+1. XXX
+2. XXX
+3.
+4.
+5.
+6.
+7.
+8.
+9. 
+10. 
+*/}
